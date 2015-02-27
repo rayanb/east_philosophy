76183560ae334d6caa4cac8d3409a76016f898ec
JeffT23 <kevalwell@gmail.com>
2015-02-27 16:59:42 -0500

    Created Seed's

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@ happy = Category.create(mood: "Happy")
 sad = Category.create(mood: "Sad")
 angry = Category.create(mood: "Angry")
 encouraged = Category.create(mood: "Encouraged")
+
 jeff = Message.create(title: "Jeff is Blitzkreig", content: "Jeff is my favorite friend, he keeps me grounded. Plus, he buys me coffee sometimes..", user: kev, category: encouraged)
