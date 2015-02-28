d51db217ca488c06c4e38b260e22a9e3c18b50d6
Rayanb <rayanbouts@gmail.com>
2015-02-28 18:18:27 -0500

    updated readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,4 @@ happy = Category.create(mood: "Happy")
 sad = Category.create(mood: "Sad")
 angry = Category.create(mood: "Angry")
 encouraged = Category.create(mood: "Encouraged")
-<<<<<<< HEAD
-
-=======
->>>>>>> c6fa0679b4963a290c66500664433c6a75366876
 jeff = Message.create(title: "Jeff is Blitzkreig", content: "Jeff is my favorite friend, he keeps me grounded. Plus, he buys me coffee sometimes..", user: kev, category: encouraged)
